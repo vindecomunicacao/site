fc7c0410c5b9114c4a4c60a5402d087245666a3d
luis.portugal <luis.portugal@cinbesa.com.br>
2017-05-24 16:53:24 -0300

    Alteracao do wifth da imagem do site em construcao para 100%, pois ficava cortada a mesma

--- resources/views/site/home/paginaconstrucao.blade.php
+++ resources/views/site/home/paginaconstrucao.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.siteconstrucao')
 @section('conteudo')
   <div>
-      <img class="img-responsive" src="images/BannerPaginaConstrucao.png" />
+      <img class="img-responsive" src="images/BannerPaginaConstrucao.png" width="100%" />
   </div>
-
 @endsection
